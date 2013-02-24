aab71b0d4a1dc2fb0b77583bbe38552de6c226ee
rinrynque <rinrynque@outlook.com>
2013-02-24 23:31:33 +0100

    Commit #1

---
+++ rinrynque_Output/coords3d.h
@@ -0,0 +1,33 @@
+#ifndef coords3d3D_H_INCLUDED
+#define coords3d3D_H_INCLUDED
+
+struct coords3d
+{
+    double x;
+    double y;
+    double z;
+
+    //Fonction static pour pouvoir l'appeler à tt moment
+    static coords3d retournercoords3d(double t_x, double t_y, double t_z);
+
+    coords3d();
+    coords3d(double t_x, double t_y, double t_z);
+
+    //Fonctions de surcharge d'opérateurs raccourcis
+    coords3d& operator+=(const coords3d &b);
+    coords3d& operator-=(const coords3d &b);
+    coords3d& operator*=(const coords3d &b);
+    coords3d& operator/=(const coords3d &b);
+};
+
+//Fonctions de surcharge d'opérateurs
+coords3d operator+(coords3d const& a, coords3d const& b);
+coords3d operator-(coords3d const& a, coords3d const& b);
+coords3d operator*(coords3d const& a, coords3d const& b);
+coords3d operator/(coords3d const& a, coords3d const& b);
+
+coords3d operator%(coords3d const& a, coords3d const& b);
+
+
+
+#endif // coords3d3D_H_INCLUDED
